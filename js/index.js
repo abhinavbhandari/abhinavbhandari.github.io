8241f77ccde8eeca346c2a591e3eca6699aaa799
Abhinav Bhandari <abhinav.bhandari@agoop.co.jp>
2019-10-04 20:17:02 +0900

    Revert "removing the resume page"
    This reverts commit 3d72fa4302644c127c7ed957901264f3348d37f2.

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ $header_top.find('a').on('click', function() {
 
 // fullpage customization
 $('#fullpage').fullpage({
-  sectionsColor: ['whitesmoke', 'whitesmoke', 'whitesmoke', 'whitesmoke', '#whitesmoke'],
+  sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
   sectionSelector: '.vertical-scrolling',
   slideSelector: '.horizontal-scrolling',
   navigation: true,
